8893752baeac04b800257caac1431ba56d4b987b
yosuakybernan314@gmail.com <yosuakybernan314@gmail.com>
2025-06-04 09:14:18 +0700

    [BAB 6] ANIMATION

--- App.jsx
+++ App.jsx
@@ -1,48 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screen/HomeScreen';
 import DetailScreen from './src/screen/DetailScreen';
 import FormScreen from './src/screen/FormScreen';
-import { Home2, AddSquare } from 'iconsax-react-native';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function HomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="HomeMain" component={HomeScreen} options={{ title: 'Beranda' }} />
-      <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
-  );
-}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
+      <Stack.Navigator
         screenOptions={{
-          tabBarActiveTintColor: '#007BFF',
-          headerShown: false,
+          animation: 'slide_from_right', // 🟢 Ini menambahkan animasi slide
         }}
       >
-        <Tab.Screen
-          name="Home"
-          component={HomeStack}
-          options={{
-            tabBarIcon: ({ color }) => <Home2 size={24} color={color} />,
-          }}
-        />
-        <Tab.Screen
-          name="Form"
-          component={FormScreen}
-          options={{
-            tabBarIcon: ({ color }) => <AddSquare size={24} color={color} />,
-          }}
-        />
-      </Tab.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Form" component={FormScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
